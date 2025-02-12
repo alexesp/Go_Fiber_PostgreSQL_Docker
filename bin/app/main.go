ec0396a03e9bcbbafbe6ed9ce3da4fd83a20d16d
alexesp <alexizaes@hotmail.com>
2025-02-12 13:36:46 +0100

    Iniciar proyecto con docker compose y dockerfile

--- bin/app/main.go
+++ bin/app/main.go
@@ -1,7 +1,24 @@
 package main
 
+import (
+	"log"
 
+	"github.com/gofiber/fiber/v2"
+)
+func welcome(c *fiber.Ctx) error{
+	return c.SendString("Welcome to may")
+}
+
+func setupRoutes(app *fiber.App){
+	
+	app.Get("/", welcome)
+}
 
 func main(){
-	
+	app := fiber.New()
+
+	//app.Get("/", welcome)
+	setupRoutes(app)
+
+	log.Fatal(app.Listen(":3000"))
 }
